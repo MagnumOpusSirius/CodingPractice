467b9989235ac77a2900ed0f0e413599b903d594
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2023-11-12 21:13:21 -0800

    update again

--- src/main/java/com/demo/streamAPI/Set2.java
+++ src/main/java/com/demo/streamAPI/Set2.java
@@ -1,10 +1,8 @@
 package com.demo.streamAPI;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Set2 {
     public static void printRandom(){
@@ -52,17 +50,32 @@ public class Set2 {
                 .filter(str -> str.length() > 4).count());
 
     }
-    //find the number of strings in a list whose length is greater than 4.
-    public static void operation3() {
-        List<String> list= Arrays.asList("abcd", "ac", "abcscs", "asd", "asdasfafa");
-        System.out.println((Long) list.stream()
-                .filter(str -> str.length() > 4).count());
+    //concatenate two streams;
+    public static void operation4() {
+        List<Integer> arr1= Arrays.asList(1,2,3,4,5);
+        List<Integer> arr2= Arrays.asList(6,7,9,10);
 
+        Stream<Integer> stream= Stream.concat(arr1.stream(), arr2.stream());
+        System.out.println(stream.collect(Collectors.toList()));
     }
-
-
+    //remove duplicate elements from the list:
+    public static void operation5() {
+        List<Integer> arr1= Arrays.asList(1,2,3,4,5,5,3,2,11);
+        System.out.println(arr1.stream().distinct().collect(Collectors.toList()));
+    }
+    //find all even and odd numbers in the list:
+    public static void operation6() {
+        List<Integer> arr1= Arrays.asList(1,2,3,4,5,5,3,2,11);
+        System.out.println(arr1.stream().filter(x->x%2==0).collect(Collectors.toList()));
+        System.out.println(arr1.stream().filter(x->x%2!=0).collect(Collectors.toList()));
+    }
+    //find the duplicate elements:
+    public static void operation7() {
+        List<Integer> arr1= Arrays.asList(1,2,3,4,5,5,3,2,11);
+        Set<Integer> set= new HashSet<>();
+        System.out.println(arr1.stream().filter(x->!set.add(x)).sorted().collect(Collectors.toList()));
+    }
     public static void main(String[] args) {
-        operation3();
-
+        operation7();
     }
 }
