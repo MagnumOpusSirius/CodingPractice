83368bad4000ff9bc3aba918ac7aa4764b4254aa
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2023-12-06 19:07:37 -0800

    added

--- src/main/java/com/demo/Java2blog/Set1.java
+++ src/main/java/com/demo/Java2blog/Set1.java
@@ -1,9 +1,6 @@
 package com.demo.Java2blog;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Set1 {
     //reverse a string without using inbuilt methods
@@ -73,22 +70,58 @@ public class Set1 {
     }
 
     //check if one string is present in another string:
-    public boolean ifStringPresentCheck()
+//    public boolean ifStringPresentCheck(String s, String t){
+//
+//    }
+    //find duplicate characters in string:
+    public static void findDuplicateChar(String str){
+        HashMap<Character, Integer> newMap= new HashMap<>();
+        for(int i=0; i<str.length(); i++){
+            newMap.put(str.charAt(i),newMap.getOrDefault(str.charAt(i),0)+1);
+        }
+//        int[] arr= new int[str.length()-1];
+        for(Character c: newMap.keySet()){
+            if(newMap.get(c)>1){
+//                arr[c]+=newMap.get(c);
+                System.out.println(c+": "+newMap.get(c));
+            }
+        }
+    }
+    //find first non-repeated character in string:
+    //example: "hhello" -> e is the first
+    public static void nonRepeatedCharacter(String str){
+        str= str.replaceAll("\\s","");
+        System.out.println(str);
+        Map<Character, Integer> newMap= new LinkedHashMap<>();
+        for(int i=0; i<str.length(); i++){
+            newMap.put(str.charAt(i), newMap.getOrDefault(str.charAt(i),0)+1);
+        }
+//        System.out.println(newMap);
+        for(Map.Entry<Character, Integer> e: newMap.entrySet()){
+            if(e.getValue()==1){
+                System.out.println(e.getKey());
+                break;
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
-        Set1 s= new Set1();
-        //=========
-        String str= "Hello World";
-        System.out.println(s.reverseString(str));
-        System.out.println(s.reverseStringUsingBuilder(str));
-        //=========
-        String s1 = "cat";
-        String t1= "cat";
-        System.out.println(s.anagram(s1,t1));
-        //=========
-        System.out.println("Unique Char: "+ s.uniqueChar(s1));
-        System.out.println("Rotation of String Check: "+ s.rotationCheck(s1,t1));
-
+//        Set1 s= new Set1();
+//        //=========
+        String str= "annalogy hey";
+//        System.out.println(s.reverseString(str));
+//        System.out.println(s.reverseStringUsingBuilder(str));
+//        //=========
+//        String s1 = "cat";
+//        String t1= "cat";
+//        System.out.println(s.anagram(s1,t1));
+//        //=========
+//        System.out.println("Unique Char: "+ s.uniqueChar(s1));
+//        System.out.println("Rotation of String Check: "+ s.rotationCheck(s1,t1));
+//        //=========
+//        findDuplicateChar(str);
+        nonRepeatedCharacter(str);
 
     }
 }
