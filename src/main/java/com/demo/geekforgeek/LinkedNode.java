e0a9e7cb8e1d8502515e8930908dd4d5098c1898
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2024-03-10 22:14:09 -0700

    first repeat and non repeating

--- src/main/java/com/demo/geekforgeek/LinkedNode.java
+++ src/main/java/com/demo/geekforgeek/LinkedNode.java
@@ -1,4 +1,17 @@
 package com.demo.geekforgeek;
 
 public class LinkedNode {
+    int val;
+    LinkedNode next;
+
+    public LinkedNode(){
+    }
+    public LinkedNode(int val){
+        this.val= val;
+    }
+
+    public LinkedNode(int val, LinkedNode next){
+        this.next= next;
+    }
+
 }
--- src/main/java/com/demo/geekforgeek/Set1.java
+++ src/main/java/com/demo/geekforgeek/Set1.java
@@ -1,6 +1,40 @@
 package com.demo.geekforgeek;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Set1 {
+    static void first_Repeating_And_First_NonRepeating_Element(int[] nums){
+        Map<Integer, Integer> map= new HashMap<>();
 
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
 
+        //find the first repeating element:
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            int key= entry.getKey();
+            int value= entry.getValue();
+            if(value>1){
+                System.out.println("first repeating: "+key);
+                break;
+            }
+
+        }
+
+        //find the first non-repeating element:
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            int key= entry.getKey();
+            int value= entry.getValue();
+            if(value==1){
+                System.out.println("first non repeating: "+key);
+                break;
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr= {1,3,5,7,8,5,3,6,7,5};
+        first_Repeating_And_First_NonRepeating_Element(arr);
+    }
 }
