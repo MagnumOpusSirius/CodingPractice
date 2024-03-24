b098f559ed97a8182571d154686ad19f489d361c
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2024-03-23 17:35:18 -0700

    solved the problem

--- src/main/java/com/demo/HackerRank/OnbeProblem2.java
+++ src/main/java/com/demo/HackerRank/OnbeProblem2.java
@@ -1,55 +1,69 @@
 package com.demo.HackerRank;
 
+import java.util.Arrays;
+
 public class OnbeProblem2 {
     public static void main(String[] args) {
-        String encryptedMessage = "cdeb nqxg ccc";
-        String knownWord = "love";
 
-        String decryptedMessage = decipher(encryptedMessage, knownWord);
-        System.out.println("Decrypted Message: " + decryptedMessage);
+        String input = "cdeb nqxg";
+        String target = "love"; //knud, jmtc
+        String[] inputArray = input.split("\\s+");
+        int shiftAmount = findShiftAmount(inputArray, target);
+        System.out.println(Arrays.toString(inputArray));
+        if (shiftAmount >= 0) {
+            System.out.println("Shift amount needed: " + shiftAmount);
+        } else {
+            System.out.println("Target string cannot be reached from the input string.");
+        }
     }
-
-    public static String decipher(String encryptedMessage, String knownWord) {
-        // Find the number of shifts to decrypt the known word
-        int shift = calculateShift(encryptedMessage, knownWord);
-
-        // Decrypt the entire message using the calculated shift
-        StringBuilder decryptedMessage = new StringBuilder();
-        for (char c : encryptedMessage.toCharArray()) {
-            if (Character.isLetter(c)) {
-                char decryptedChar = (char) (c - shift);
-                if ((Character.isLowerCase(c) && decryptedChar < 'a') || (Character.isUpperCase(c) && decryptedChar < 'A')) {
-                    decryptedChar += 26;
-                }
-                decryptedMessage.append(decryptedChar);
-            } else {
-                decryptedMessage.append(c);
+    public static int findShiftAmount(String[] input, String target) {
+        // Check if any input string is already equal to the target string
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].equals(target)) {
+                return 0; // No shift needed, as the input string already matches the target string
             }
         }
-
-        return decryptedMessage.toString();
-    }
-
-    public static int calculateShift(String encryptedMessage, String knownWord) {
-        int shift = 0;
-
-        for (int i = 0; i <= encryptedMessage.length() - knownWord.length(); i++) {
-            String substring = encryptedMessage.substring(i, i + knownWord.length());
-            int currentShift = 0;
-            for (int j = 0; j < knownWord.length(); j++) {
-                int encryptedAscii = Character.toLowerCase(substring.charAt(j)) - 'a';
-                int knownAscii = Character.toLowerCase(knownWord.charAt(j)) - 'a';
-                currentShift = (encryptedAscii - knownAscii + 26) % 26;
-                if (currentShift != 2) {
-                    break; // If shift is not 2, move to the next substring
+        // Iterate through possible shift amounts
+        for (int shiftAmount = 1; shiftAmount < 26; shiftAmount++) {
+            for(String word: input){
+                String shiftedRight = shiftAlphabetically(word, shiftAmount);
+                String shiftedLeft = shiftAlphabetically(word, -shiftAmount);
+                if (shiftedRight.equals(target)) {
+                    shiftArray(input, shiftAmount);
+                    return shiftAmount; // Found the matching shift amount
+                } else if (shiftedLeft.equals(target)) {
+                    shiftArray(input, -shiftAmount);
+                    return shiftAmount; // Found the matching shift amount
                 }
             }
-            if (currentShift == 2) {
-                shift = currentShift;
-                break; // If shift is 2, we found the correct shift, no need to check further
-            }
         }
+        return -1; // Target string not found
+    }
+    public static void shiftArray(String[] array, int shiftAmount) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = shiftAlphabetically(array[i], shiftAmount);
+        }
+    }
+    public static String shiftAlphabetically(String input, int shiftAmount){
 
-        return shift;
+        int length = input.length();
+        shiftAmount=shiftAmount%length;
+        char[] shiftedChars = new char[input.length()];
+        for(int i =0; i<length; i++){
+            char currentChar = input.charAt(i);
+            char shiftedChar = shiftCharacter(currentChar, shiftAmount);
+
+            shiftedChars[i] = shiftedChar;
+        }
+        return new String(shiftedChars);
+    }
+
+    public static char shiftCharacter(char c, int shiftAmount) {
+        int shiftedValue = (c - 'a' + shiftAmount) % 26;
+        if (shiftedValue < 0) {
+            shiftedValue += 26; // Wrap around to handle negative values
+        }
+        char shiftedChar = (char) (shiftedValue + 'a');
+        return shiftedChar;
     }
 }
