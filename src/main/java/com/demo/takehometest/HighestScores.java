9dc07d56e5b342099fe7784ab9b0c9d727c9adfd
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2024-04-15 15:58:42 -0700

    added

---
+++ src/main/java/com/demo/takehometest/HighestScores.java
@@ -0,0 +1,2 @@
+package com.demo.takehometest;public class HighestScores {
+}
---
+++ src/main/java/com/demo/takehometest/Records.java
@@ -0,0 +1,26 @@
+package com.demo.takehometest.record;
+import org.json.JSONObject;
+public class Records {
+    private int score;
+    private String id;
+
+    public Records(int score, String id){
+        this.score= score;
+        this.id= id;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public JSONObject toJson(){
+        JSONObject json= new JSONObject();
+        json.put("score", score);
+        json.put("id", id);
+        return json;
+    }
+}
